ee38bb7302091ffeed42bfaa0ae70eba721a09e0
Michael Smith <homelywingsv2@gmail.com>
2022-09-14 16:55:46 -0400

    Initial Commit

---
+++ bioTech/bioTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  bioTechApp.swift
+//  bioTech
+//
+//  Created by Naeyma Ahmed on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct bioTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
