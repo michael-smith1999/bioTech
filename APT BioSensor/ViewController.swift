26a3cbe4e4e28c48f1d8995512eeacf2b357e652
tjosephm <Mulligant99@gmail.com>
2022-10-04 16:34:08 -0400

    Split variable to reduce possible mistakes

--- APT BioSensor/ViewController.swift
+++ APT BioSensor/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var pitch: UILabel!
     @IBOutlet weak var yaw: UILabel!
     let conVal = 180/Double.pi
-    var tempVal:Double = 0
+    var rollDeg:Double = 0
+    var yawDeg:Double = 0
+    var pitchDeg:Double = 0
     
     var motion = CMMotionManager()
 
@@ -29,14 +31,14 @@ class ViewController: UIViewController {
         motion.startDeviceMotionUpdates(to: OperationQueue.current!){ (data, error) in
             if let trueData = data{
                 //convert roll, pitch, yaw to degrees and display
-                self.tempVal=trueData.attitude.roll*self.conVal
-                self.roll.text = "\(self.tempVal)"
+                self.rollDeg=trueData.attitude.roll*self.conVal
+                self.roll.text = "\(self.rollDeg)"
                 
-                self.tempVal=trueData.attitude.yaw*self.conVal
-                self.yaw.text = "\(self.tempVal)"
+                self.yawDeg=trueData.attitude.yaw*self.conVal
+                self.yaw.text = "\(self.yawDeg)"
                 
-                self.tempVal=trueData.attitude.pitch*self.conVal
-                self.pitch.text = "\(self.tempVal)"
+                self.pitchDeg=trueData.attitude.pitch*self.conVal
+                self.pitch.text = "\(self.pitchDeg)"
             }
             
         }
