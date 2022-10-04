dd5286851fe73da1ecfbb47ebe4b59189ea1ef0b
Daniel Pinto <daniel.pinto@ufl.edu>
2022-10-04 17:21:51 -0400

    implementing initial notifications functionality

--- APT BioSensor/ViewController.swift
+++ APT BioSensor/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreMotion
+import UserNotifications
 
 class ViewController: UIViewController {
     
@@ -20,6 +21,35 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         getOrientation();
         // Do any additional setup after loading the view.
+        
+        // Notifications:
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Time for your daily measurements!"
+        content.body = "Please open the APT BioSensor app for your daily pelvic tilt and pain measurements"
+        
+        // Step 3: Create the notification trigger
+        let date = Date().addingTimeInterval(5) // adds a time interval of 10 seconds to date
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
     }
     func getOrientation() {
         
