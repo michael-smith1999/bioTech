4b29d4a887110a7ce30de15f908bbbf75a332f58
larrylim1996 <76197142+larrylim1996@users.noreply.github.com>
2023-01-23 18:35:33 +0800

    Changed from Storyboard to SwiftUI
    Totally new code... First code after SwiftUI online training.

---
+++ BioSensor/BioSensor/ContentView.swift
@@ -0,0 +1,252 @@
+//
+//  ContentView.swift
+//  BioSensor
+//
+//  Created by Shi Xiang Lim on 1/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+
+    @State private var readings =        [
+        Reading(
+            date:"01/02/03",
+            interval:"Test"
+        )
+        ]
+    
+    
+    @State private var tabSelection = 1
+    @State private var buttonPressed = 0
+    @State private var currentDate = Date.now
+    @State private var textInput = ""
+    var body: some View {
+        TabView (selection: $tabSelection){
+            MainMenu(tabSelection: $tabSelection)
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            
+                .tag(1)
+            
+            Measurement(currentDate:$currentDate,
+                        reading:$readings, textInput:$textInput)
+                .tabItem {
+                    Label("Measurement", systemImage: "square.and.pencil")
+                }
+                .tag(2)
+            Notification(currentDate:$currentDate)
+                .tabItem {
+                    Label("Notification", systemImage: "bell.and.waves.left.and.right")
+                }
+                .tag(3)
+            QuickStartGuide()
+                .tabItem {
+                    Label("Quick Start", systemImage: "gear.badge.questionmark")
+                }
+                .tag(4)
+        }
+    }
+}
+
+struct Reading: Identifiable {
+    let date: String
+    let interval:String
+    let id = UUID()
+}
+
+struct MainMenu: View {
+    @Binding var tabSelection: Int
+    var body: some View {
+        ZStack{
+            /*
+            LinearGradient(
+                                colors: [.blue, .yellow],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+            ).edgesIgnoringSafeArea([.top])*/
+            Color.teal.edgesIgnoringSafeArea([.top])
+            VStack {
+                Spacer()
+                Text("APT BioSensor")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.black)
+                Spacer()
+                
+                Button(action: {
+                    print("Pressed!")
+                }){
+                    Text("Start Measurement")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: UIScreen.main.bounds.width*3/4, height: UIScreen.main.bounds.width*3/4)
+                        .background(RadialGradient(gradient: Gradient(colors: [.gray, .black]), center: .center, startRadius:UIScreen.main.bounds.width/3 , endRadius: UIScreen.main.bounds.width*3/8))
+                        .clipShape(Circle())
+                }.buttonStyle(PlainButtonStyle())
+                Spacer()
+                
+                Text("Quick Start Guide")
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .underline()
+                    .onTapGesture {
+                        self.tabSelection=4
+                    }
+                Spacer()
+            }
+        }
+        
+    }
+}
+
+struct Measurement: View {
+    @Binding var currentDate:Date
+    @Binding var reading:[Reading]
+    @Binding var textInput:String
+    var body: some View {
+        ZStack{
+            Color.teal.edgesIgnoringSafeArea([.top])
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    DatePicker("",
+                               selection: $currentDate,displayedComponents: .date)
+                        .labelsHidden()
+                        .frame(width: 150, height: 80, alignment: .center)
+                        .clipped()
+                    
+                    Spacer()
+                }
+                Spacer()
+                TextEditor(text: .constant("Placeholder for chart"))
+                    .padding(.horizontal)
+                HStack{
+                    Spacer()
+                    /*
+                    Table(reading) {
+                        TableColumn("Date",value: \.date)
+                        TableColumn("Time Interval (s)", value:\.interval)
+                    }
+                    */
+                    List {
+                        HStack{
+                            Text("Content1")
+                            Spacer()
+                            Text("Value")
+                        }
+                        HStack{
+                            Text("Content1")
+                            Spacer()
+                            Text("Value")
+                        }
+                    }
+                    Spacer()
+                }
+                Spacer()
+                HStack{
+                    Spacer()
+                    TextField("Delete row",text:$textInput)
+                        .font(.subheadline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: UIScreen.main.bounds.width*5/10, height: UIScreen.main.bounds.width*1/10)
+                        .background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                    
+                    Spacer()
+                    
+                    Button("Delete") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
+                    .font(.subheadline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                        .frame(width: UIScreen.main.bounds.width*5/10, height: UIScreen.main.bounds.width*1/10)
+                        .background(Color.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 30.0))
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Notification: View {
+    @Binding var currentDate:Date
+    var body: some View {
+        ZStack{
+            Color.teal.edgesIgnoringSafeArea([.top])
+            VStack{
+                HStack{
+                    Text("Notification")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding(.leading)
+                    Spacer()
+                    Button("+"){} .padding(.trailing).foregroundColor(Color.black).buttonBorderShape(ButtonBorderShape.roundedRectangle)
+                        .frame(width: UIScreen.main.bounds.width*1/8, height: UIScreen.main.bounds.width*1/8)
+                        .background(Color.gray.edgesIgnoringSafeArea([]))
+                        
+                    Button("-"){} .padding().foregroundColor(Color.black).buttonBorderShape(ButtonBorderShape.roundedRectangle)
+                        .frame(width: UIScreen.main.bounds.width*1/8, height: UIScreen.main.bounds.width*1/8)
+                        .background(Color.gray.edgesIgnoringSafeArea([]))
+
+                }
+                Spacer()
+                List {
+                        HStack{
+                            DatePicker("",
+                                       selection: $currentDate,displayedComponents: .hourAndMinute)
+                                .labelsHidden()
+                                .frame(width: 150, height: 80, alignment: .leading)
+                                .clipped()
+                            Spacer()
+                            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                               
+                            }
+                        }
+                    HStack{
+                        DatePicker("",
+                                   selection: $currentDate,displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                            .frame(width: 150, height: 80, alignment: .leading)
+                            .clipped()
+                        Spacer()
+                        Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                            
+                        }
+                    }
+                }
+                Spacer()
+            }
+            
+        }
+    }
+}
+
+struct QuickStartGuide: View {
+    var body: some View {
+        ZStack{
+            Color.teal.edgesIgnoringSafeArea([.top])
+            VStack{
+                Text("This place is for information, ideally in list view")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
